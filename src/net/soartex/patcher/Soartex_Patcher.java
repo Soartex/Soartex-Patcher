3fe3efc522dab9635327e026892b3f5522941c18
REDX36 <redx36a@live.com>
2012-09-24 10:27:18 -0400

    Non-commit
    Gonna go to Swing and away from SWT for this project, we really need the
    multi-threading and size capabilities.

--- src/net/soartex/patcher/Soartex_Patcher.java
+++ src/net/soartex/patcher/Soartex_Patcher.java
@@ -7,9 +7,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
 import java.net.URL;
 
 import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
